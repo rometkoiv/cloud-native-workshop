b500859f31f65aaef04ac0cd786dada9033999f6
Romet Kõiv <romet.koiv@smit.ee>
2017-11-13 11:35:58 +0200

    Punkt kolm

--- 12f_app/src/main/java/com/github/rometkoiv/app/StockResource.java
+++ 12f_app/src/main/java/com/github/rometkoiv/app/StockResource.java
@@ -1,13 +1,24 @@
 package com.github.rometkoiv.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/stock", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StockResource {
+	@Autowired
+	private Environment environment;
+
+	@RequestMapping(value = "/get-env", method = RequestMethod.GET)
+	public String getEnv(@RequestParam String env) {
+	    return "Env" + " " + environment.getProperty(env);
+	}
+
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
         return "Ping";
