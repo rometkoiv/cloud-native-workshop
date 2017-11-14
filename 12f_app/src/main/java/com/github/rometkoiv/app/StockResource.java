338199e6113b5b3da6501539fdb7e0f43fa9f8fe
Romet Kõiv <romet.koiv@smit.ee>
2017-11-14 10:39:39 +0200

    Yksteist

--- 12f_app/src/main/java/com/github/rometkoiv/app/StockResource.java
+++ 12f_app/src/main/java/com/github/rometkoiv/app/StockResource.java
@@ -2,6 +2,8 @@ package com.github.rometkoiv.app;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -23,7 +25,9 @@ public class StockResource {
 	private Environment environment;
     @Autowired
     private StockItemRepository stockItemRepository;
-
+    
+    private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
+    
 	@RequestMapping(value = "/get-env", method = RequestMethod.GET)
 	public String getEnv(@RequestParam String env) {
 	    return "Env" + " " + environment.getProperty(env);
@@ -38,13 +42,17 @@ public class StockResource {
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
 
+    	logger.info("Starting search of stock item(id={}) search", id);
+
         StockItem stockItem = stockItemRepository.findOne(id);
 
         if (stockItem == null) {
+            logger.info("Stock item(id={}) has not been found", id);
             response.setStatus(HttpStatus.NOT_FOUND.value());
             return null;
         }
 
+        logger.info("Finishing search of stock item(id={}) search", id);
         return stockItem;
     }
 
